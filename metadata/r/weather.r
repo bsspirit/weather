3ed94083e4730bb360116bc254d1742ada8bf60a
bsspirit <bsspirit@163.com>
2013-02-06 16:49:49 +0800

    summary

--- metadata/r/weather.r
+++ metadata/r/weather.r
@@ -183,6 +183,67 @@ atmosphere<-function(data=data,type='humidity',output=FALSE,path=''){
   if(output)dev.off()
 }
 
+getColors2<-function(map,prov,ctype){
+  #name change to ADCODE99
+  ADCODE99<-read.csv(file="ADCODE99.csv",header=TRUE,fileEncoding="utf-8", encoding="utf-8")
+  fc<-function(x){ADCODE99$ADCODE99[which(x==ADCODE99$prov)]}
+  code<-sapply(prov,fc)
+  
+  f=function(x,y) ifelse(x %in% y,which(y==x),0);
+  colIndex=sapply(map$ADCODE99,f,code);
+  ctype[which(is.na(ctype))]=19
+  return(ctype[colIndex])
+}
+
+summary<-function(data=data,output=FALSE,path=''){
+  library("RColorBrewer")
+  colors<-c(rev(brewer.pal(9,"Blues")),rev(c('#b80137','#8c0287','#d93c5d','#d98698','#f6b400','#c4c4a7','#d6d6cb','#d1b747','#ffeda0')))
+  
+  temp<-data$code
+  title<-"中国各省天气概况"
+  ofile<-paste(format(date,"%Y%m%d"),"_code.png",sep="")
+  sign<-''
+  colors<-rev(colors)
+  code<-read.csv(file="code.csv",header=TRUE,fileEncoding="utf-8", encoding="utf-8")
+  labelcode<-read.csv(file="labelcode.csv",header=TRUE,fileEncoding="utf-8", encoding="utf-8")
+  ctype<-sapply(temp,function(x){code$type[which(x==code$code)]})
+  
+  if(output)png(file=paste(path,ofile,sep=''),width=600,height=600)
+  layout(matrix(data=c(1,2),nrow=1,ncol=2),widths=c(8,1),heights=c(1,2))
+  par(mar=c(0,0,3,12),oma=c(0.2,0.2,0.2,0.2),mex=0.3)
+  plot(map,border="white",col=colors[getColors2(map,data$prov,ctype)])
+  points(data$long,data$lat,pch=19,col=rgb(0,0,0,0.3),cex=0.8)
+  
+  #=======================================
+  if(FALSE){
+    grid()
+    axis(1,lwd=0);axis(2,lwd=0);axis(3,lwd=0);axis(4,lwd=0)
+  }
+  text(100,58, title,cex=2)
+  text(105,54,format(date,"%Y-%m-%d"))
+  text(120,-8,paste('provided by The Weather Channel',format(date, "%Y-%m-%d %H:%M")),cex=0.8)
+  
+  #=======================================
+  for(row in 1:nrow(data)){
+    name<-as.character(data$zh[row])
+    label<-labelcode$alias[labelcode$type==ctype[row]]
+    x1<-ceiling(row/7)
+    x2<-ifelse(row%%7==0,7,row%%7)
+    x3<-ctype[row]
+    fontCol<-'#000000'
+    if(x3<=5)fontCol<-head(colors,1)
+    if(x3>=12)fontCol<-tail(colors,1)
+    text(68+x1*11,17-x2*3,paste(name,' ',label,sign,sep=''),col=fontCol)
+  }
+  
+  #=======================================
+  par(mar = c(5, 0, 15, 10))
+  image(x=1, y=1:length(colors),z=t(matrix(1:length(colors))),col=rev(colors),axes=FALSE,xlab="",ylab="",xaxt="n")
+  axis(4, at = 1:(nrow(labelcode)-1), labels=rev(labelcode$alias)[-1], col = "white", las = 1)
+  abline(h=c(1:(nrow(labelcode)-2)+0.5), col = "white", lwd = 2, xpd = FALSE)
+  if(output)dev.off()
+}
+
 date=Sys.time()
 loadDate(date)
 
@@ -194,3 +255,4 @@ weather(data,type='low',output=TRUE,path=path)
 atmosphere(data,type='humidity',output=TRUE,path=path)
 atmosphere(data,type='pressure',output=TRUE,path=path)
 atmosphere(data,type='visibility',output=TRUE,path=path)
+summary(data,output=TRUE,path=path)
